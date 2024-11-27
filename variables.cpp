cca59da81977762d72c77d5f4f32dd9c0d1b987b
doingcodelikeashit <doingcodelikeashit@gmail.com>
2024-11-27 20:46:15 +0530

    Day-1 datatypes and variables

---
+++ variables.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=1;
+    float b=3.14;
+    char c='a';
+    double b2=3.123123123123123123123123123;
+    bool d=true; //1=true and 0=false
+}
