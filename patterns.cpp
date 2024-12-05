94a5f793e354af61e8fdeb40c0694b6dcbc6889b
doingcodelikeashit <doingcodelikeashit@gmail.com>
2024-12-05 21:31:32 +0530

    Day-7 Home work pattern and triangle patterns

--- patterns.cpp
+++ patterns.cpp
@@ -25,4 +25,38 @@ int main(){
        }
        cout<<endl;
     }
+
+    cout<<"\n1234..N square (2) :: \n";
+    int count=1;
+    for (int i = 1; i <= n; i++)
+    {
+       for (int j = 1; j <= n; j++)
+       {
+            cout<<count<<" ";
+            count++;
+       }
+       cout<<endl;
+    }
+
+    //Home work pattern
+    cout<<"\nABCD..N square :: \n";
+    char ch='A';
+    for (int i = 1; i <= n; i++)
+    {   
+       for (int j = 1; j <= n; j++)
+       {
+            cout<<ch<<" "; // you can print * also
+            ch++;
+       }
+       cout<<endl;
+    }
+
+    //triangle pattern
+    cout<<"\n * triangle :: \n";
+    for(int i=0;i<n;i++){
+     for(int j=0;j<i+1;j++){
+          cout<<"* ";
+     }
+     cout<<endl;
+    }
 }
