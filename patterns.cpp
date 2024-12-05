10e456f8557084574ae37bae2d25f8462566c859
doingcodelikeashit <doingcodelikeashit@gmail.com>
2024-12-05 22:18:33 +0530

    Day-7 triangle pattern and pyramid pattern

--- patterns.cpp
+++ patterns.cpp
@@ -59,4 +59,70 @@ int main(){
      }
      cout<<endl;
     }
+
+    //numbers triangle
+    cout<<"\n numbers triangle :: \n";
+    for(int i=0;i<n;i++){
+     for(int j=0;j<i+1;j++){
+          cout<<(i+1)<<" ";
+     }
+     cout<<endl;
+    }
+
+    cout<<"\n numbers triangle (2) :: \n";
+    for(int i=0;i<n;i++){
+     for(int j=0;j<i+1;j++){
+          cout<<(j+1)<<" ";
+     }
+     cout<<endl;
+    }
+
+    //reverse triangle pattern
+    cout<<"\n reverse triangle pattern :: \n";
+    for(int i=0;i<n;i++){
+     for(int j=i+1;j>0;j--){
+          cout<<j<<" ";
+     }
+     cout<<endl;
+    }
+
+    cout<<"\n floyds triangle :: \n";
+    count=1;
+    for(int i=0;i<n;i++){
+     for(int j=0;j<i+1;j++){
+          cout<<count<<" ";
+          count++;
+     }
+     cout<<endl;
+    }
+
+    //inverted triangle pattern
+    cout<<"\n inverted triangle pattern :: \n";
+    for (int i = 0; i < n; i++)
+    {
+          for(int j = 0;j < i;j++){
+               cout<<" ";
+          }
+          for(int k = 0;k<n-i;k++){
+               cout<<(i+1);
+          }
+          cout<<endl;
+    }
+
+    //pyramid pattern
+    cout<<"\npyramid triangle pattern :: \n";
+    for (int i = 0; i < n; i++)
+    {
+          for(int j = 0;j < n-i-1;j++){
+               cout<<" ";
+          }
+          for(int j = 1;j<=i+1;j++){
+               cout<<j;
+          }
+          for(int j=i;j>0;j--){
+               cout<<j;
+          }
+          cout<<endl;
+    }
+    
 }
